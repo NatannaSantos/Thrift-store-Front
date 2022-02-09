0fc51e135cc316ff36e428b245aaa10aacfd78b6
Natanna <natannacortez@gmail.com>
2022-02-09 10:16:39 -0300

    feat: basic code structure

---
+++ src/services/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const BASE_URL = "http://localhost:5000";
+
+function createConfig(token) {
+    return { headers: { Authorization: `Bearer ${token}` } };
+}
+
+function login(body) {
+    const promise = axios.post(`${BASE_URL}/auth/login`, body);
+    return promise;
+}
+
+function register(body) {
+    const promise = axios.post(`${BASE_URL}/auth/register`, body);
+    return promise;
+}
+
+const api={
+    login,
+    register
+}
+export default api;
